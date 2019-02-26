3a9bdb3e78c5e0972e8ed042f9b635ed529b1777
MuriloDutra <murilo.dutra2000@gmail.com>
2019-02-26 18:12:04 -0300

    Pagina 'detalhes-fornecedor' finalizada

--- detalhes-fornecedor.php
+++ detalhes-fornecedor.php
@@ -10,48 +10,48 @@
             <table class="table">
                 <tr>
                     <td>Nome</td>
-                    <td><input class="form-control" type="text" name="nome" value="<?=$fornecedor['nome']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="nome" value="<?=$fornecedor['nome']?>"></td>
                 </tr>
                 <tr>
                     <td>E-mail</td>
-                    <td><input class="form-control" type="text" name="email" value="<?=$fornecedor['email']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="email" value="<?=$fornecedor['email']?>"></td>
                 </tr>
                 <tr>
                     <td>Telefone</td>
-                    <td><input class="form-control" type="text" name="telefone" value="<?=$fornecedor['telefone']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="telefone" value="<?=$fornecedor['telefone']?>"></td>
                 </tr>
                 <tr>
                     <td>CEP</td>
-                    <td><input class="form-control" type="text" name="cep" value="<?=$fornecedor['CEP']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="cep" value="<?=$fornecedor['CEP']?>"></td>
                 </tr>
                 <tr>
                     <td>Rua</td>
-                    <td><input class="form-control" type="text" name="rua" value="<?=$fornecedor['rua']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="rua" value="<?=$fornecedor['rua']?>"></td>
                 </tr>
                 <tr>
                     <td>Bairro</td>
-                    <td><input class="form-control" type="text" name="bairro" value="<?=$fornecedor['bairro']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="bairro" value="<?=$fornecedor['bairro']?>"></td>
                 </tr>
                 <tr>
                     <td>Município</td>
-                    <td><input class="form-control" type="text" name="municipio" value="<?=$fornecedor['municipio']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="municipio" value="<?=$fornecedor['municipio']?>"></td>
                 </tr>
                 <tr>
                     <td>Estado</td>
-                    <td><input class="form-control" type="text" name="estado" value="<?=$fornecedor['estado']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="estado" value="<?=$fornecedor['estado']?>"></td>
                 </tr>
                 <tr>
                     <td>País</td>
-                    <td><input class="form-control" type="text" name="pais" value="<?=$fornecedor['pais']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="pais" value="<?=$fornecedor['pais']?>"></td>
                 </tr>
                 <tr>
                     <td>Responsável de vendas</td>
-                    <td><input class="form-control" type="text" name="responsavelVendas" value="<?=$fornecedor['responsavelVendas']?>">
+                    <td><input class="form-control" type="text" disabled name="responsavelVendas" value="<?=$fornecedor['responsavelVendas']?>">
                 </td>
                 </tr>
                 <tr>
                     <td>CNPJ/CPF</td>
-                    <td><input class="form-control" type="text" name="cnpj_cpf" value="<?=$fornecedor['CNPJ_CPF']?>"></td>
+                    <td><input class="form-control" type="text" disabled name="cnpj_cpf" value="<?=$fornecedor['CNPJ_CPF']?>"></td>
                 </tr>
             </table>
         </form>
