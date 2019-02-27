18f6704eb23ce72f7da35a1c1c72c9c5254259d5
MuriloDutra <murilo.dutra2000@gmail.com>
2019-02-26 22:09:53 -0300

    Implementando funcao que insere no banco

--- database/banco-fornecedor.php
+++ database/banco-fornecedor.php
@@ -34,4 +34,12 @@
         $query = "delete from FORNECEDOR where id = {$id}";
         return mysqli_query($connection, $query);
     }
-?>
+
+    function insereFornecedor($connection, $nome, $email, $telefone, $cep, $rua, $bairro, $municipio, $estado, $pais, 
+    $responsavelVendas, $cnpj_cpf)
+    {
+        $query = "insert into FORNECEDOR (nome, email, telefone, CEP, rua, bairro, municipio, estado, pais, responsavelVendas, CNPJ_CPF)
+        values('{$nome}', '{$email}', {$telefone}, {$cep}, '{$rua}', '{$bairro}', '{$municipio}', {$estado}', '{$pais}', '{$responsavelVendas}', {$cnpj_cpf}";
+        
+        return mysqli_query($connection, $query);
+    }
